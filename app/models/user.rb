7c5216652436b73f3a672391e51999c2db9be1fb
Deepakdanger <depk.96.kumar@gmail.com>
2021-07-01 16:01:32 +0530

    Add vote in article index

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
 
   has_many :articles
+  has_many :votes, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :articles
+  resources :articles do
+    resources :votes, only: [:create, :destroy]
+  end
   root "articles#index"
 end
