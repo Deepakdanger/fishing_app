22290744f827257dee34e24bdb667100c1198438
Deepakdanger <depk.96.kumar@gmail.com>
2021-07-06 14:57:53 +0530

    Add popular article

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,8 @@
 module ArticlesHelper
+    def max_vote(articles)
+        arr=articles.votes.count
+        if article.last
+            return 
+        end
+    end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,6 @@ class Vote < ApplicationRecord
     validates :user_id, uniqueness: { scope: :article_id }
   belongs_to :user
   belongs_to :article
+
+  scope :popular, -> { select('article_id, count(article_id) as count').group(:article_id).order('count desc').limit(1) }
 end
