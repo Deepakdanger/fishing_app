8d93ac882029d60a64fe9ad12f1d6987952147a1
Deepakdanger <depk.96.kumar@gmail.com>
2021-07-08 15:53:24 +0530

    Add logo

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ApplicationRecord
         too_long: '500 characters in post is the maximum allowed.' }
     validates :title, presence: true, length: { maximum: 100 }
     validates :image, presence: true
+    validates :category_id, presence: true
 
     belongs_to :user
     belongs_to :category
