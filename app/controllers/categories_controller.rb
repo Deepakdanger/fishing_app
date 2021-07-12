279177789cf9e7804938c53f7df3fd57e5e8be00
Deepakdanger <depk.96.kumar@gmail.com>
2021-07-12 14:51:04 +0530

    Add category creation page and done logic

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,23 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to new_article_path, notice: 'Category is successfully created'
+    else
+      render :new, notice: 'Failed to create category'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     resources :votes, only: [:create, :destroy]
   end
   root "articles#index"
-  resources :categories, only: [:show]
+  resources :categories
 end
