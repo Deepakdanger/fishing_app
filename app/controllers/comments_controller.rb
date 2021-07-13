6a36d6823bff92b20acc0a875b55c2aa26bbd7b4
Deepakdanger <depk.96.kumar@gmail.com>
2021-07-13 11:06:58 +0530

    Add comment in article show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to articles_path, notice: 'Comment was successfully created.'
+    else
+      redirect_to articles_path, alert: @comment.errors.full_messages.join('. ').to_s
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
